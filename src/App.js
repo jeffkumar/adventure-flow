ec2e0a3b083527f1202b9335acfd7afe5fbf4f80
Jeff Kumar <jeffkumar.aw@gmail.com>
2024-09-07 08:50:01 -0600

    trying to update domain

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router  basename="/adventure-flow/">
+    <Router>
       <div className="flex flex-col min-h-screen">
         {/* Main Content */}
         <div className="flex-grow">
