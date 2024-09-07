5d1f937988ddf84ff3f5f4c46adb2bbf44271ea4
Jeff Kumar <jeffkumar.aw@gmail.com>
2024-09-07 08:42:50 -0600

    update basepath

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router  basename="/adventure-flow/">
       <div className="flex flex-col min-h-screen">
         {/* Main Content */}
         <div className="flex-grow">
